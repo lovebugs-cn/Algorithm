5c70b54055bae462e1cc78a37d14ef020ec5f229
Josiah <1319164238@qq.com>
2017-11-14 16:18:37 +0800

    向链表末尾添加一个节点

---
+++ addToTail.cpp
@@ -0,0 +1,27 @@
+//向链表的末尾添加一个节点
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+struct ListNode{
+    int p_value;
+    ListNode *p_next;
+};
+
+void addToTail(ListNode **pHead,int value){
+
+    ListNode *pNew = new ListNode();
+    pNew->p_value = value;
+    pNew->p_next = nullptr;
+
+    if(*pHead == NULL){
+        *pHead = pNew;
+    }
+    else{
+        ListNode *pNode = *pHead;
+        while(pNode->p_next!=NULL){
+           pNode = pNode->p_next;
+        }
+        pNode->p_next = pNew;
+    }
+}
